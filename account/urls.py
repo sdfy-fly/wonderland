16cee57c677626911eb8de8382068e2fc90e5079
sdfy-fly <rozhov25@bk.ru>
2023-03-03 11:47:21 +0300

    account: get, search, put, delete

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, re_path
-from .views import Registration, GetAccount, SearchAccount
+from .views import Registration, AccountService, SearchAccount
 
 urlpatterns = [
-    re_path(r'^accounts/(?P<accountId>-?\d+)/$', GetAccount.as_view()),
+    re_path(r'^accounts/(?P<accountId>-?\d+)/$', AccountService.as_view()),
     path('accounts/search', SearchAccount.as_view()),
     path('registration', Registration.as_view()),
 ]
--- account/views.py
+++ account/views.py
@@ -12,7 +12,7 @@ import base64
 
 class CheckAuth:
 
-    def __decodeCredentials(self, request):
+    def decodeCredentials(self, request):
         try:
             encoded_str = request.META.get('HTTP_AUTHORIZATION', '').split('Base')[1]
             decoded_str = base64.b64decode(encoded_str).decode("utf-8")
@@ -22,7 +22,7 @@ class CheckAuth:
             return None
 
     def checkAuth(self, request):
-        credentials = self.__decodeCredentials(request)
+        credentials = self.decodeCredentials(request)
 
         if not credentials:
             return 401
@@ -34,11 +34,86 @@ class CheckAuth:
         return 200
 
 
-class GetAccount(APIView):
+class AccountService(APIView):
     serializer_class = AccountSerializer
 
     def get(self, request, accountId):
         return self.getAccount(request, accountId)
+
+    def put(self, request, accountId):
+        return self.updateAccount(request, accountId)
+
+    def delete(self, request, accountId):
+        return self.deleteAccount(request, accountId)
+
+    def checkAccess(self, request, accountId):
+
+        accountId = int(accountId)
+        if not accountId or accountId <= 0:
+            return 400
+
+        if CheckAuth().checkAuth(request) == 401:
+            return 401
+
+        # Аккаунт не найден
+        try:
+            user = Account.objects.get(pk=accountId)
+        except Account.DoesNotExist:
+            return Response(status=403)
+
+        # Обновление не своего аккаунта
+        credentials = CheckAuth().decodeCredentials(request)
+        if credentials["email"] != user.email or credentials["password"] != user.password:
+            return Response(status=403)
+
+        return 200
+
+    def deleteAccount(self, request, accountId):
+
+        # Проверка токена,account id, и собственности
+        status = self.checkAccess(request, accountId)
+        if status != 200:
+            return Response(status=status)
+
+        #FIXME: Аккаунт связан с животным - вернуть 400
+
+        Account.objects.filter(pk=accountId).delete()
+        return Response(status=200)
+
+
+    def updateAccount(self, request, accountId):
+
+        firstName = self.request.query_params.get('firstName', None)
+        lastName = self.request.query_params.get('lastName', None)
+        email = self.request.query_params.get('email', None)
+        password = self.request.query_params.get('password', None)
+
+        # Проверка полей
+        if not firstName or len(firstName.split()) == 0 or not lastName or len(lastName.split()) == 0:
+            return Response(status=400)
+        if not email or len(email.split()) == 0 or not re.match(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", email):
+            return Response(status=400)
+        if not password or len(password.split()) == 0:
+            return Response(status=400)
+
+        # Проверка токена,account id, и собственности
+        status = self.checkAccess(request, accountId)
+        if status != 200:
+            return Response(status=status)
+
+        # Проверка уникальности email
+        if Account.objects.filter(email=email).exists():
+            return Response(status=409)
+
+        user = Account.objects.get(pk=accountId)
+        user.firstName = firstName
+        user.lastName = lastName
+        user.email = email
+        user.password = password
+        user.save()
+
+        serializer = self.serializer_class(user)
+        return Response(serializer.data, status=200)
 
     def getAccount(self, request, accountId):
 
@@ -49,8 +124,7 @@ class GetAccount(APIView):
         if CheckAuth().checkAuth(request) == 401:
             return Response(status=401)
 
-        user = Account.objects.filter(pk=accountId).exists()
-        if not user:
+        if not Account.objects.filter(pk=accountId).exists():
             return Response(status=404)
 
         serializer = self.serializer_class(Account.objects.get(pk=accountId))
@@ -75,7 +149,7 @@ class SearchAccount(generics.ListAPIView):
         if email:
             queryset = queryset.filter(Q(email__icontains=email))
 
-        return queryset[int(start):int(start) + int(size)]
+        return queryset[int(start):int(start) + int(size)][::-1]
 
     def get(self, request, *args, **kwargs):
         start = self.request.query_params.get('from', 0)
@@ -109,11 +183,12 @@ class Registration(APIView):
         if CheckAuth().checkAuth(request) == 200:
             return Response(status=403)
 
-        if not firstName or len(firstName.split()) == 0 or \
-                not lastName or len(lastName.split()) == 0 or \
-                not email or len(email.split()) == 0 or \
-                not re.match(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", email) or \
-                not password or len(password.split()) == 0:
+        # Проверка полей
+        if not firstName or len(firstName.split()) == 0 or not lastName or len(lastName.split()) == 0:
+            return Response(status=400)
+        if not email or len(email.split()) == 0 or not re.match(r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?", email):
+            return Response(status=400)
+        if not password or len(password.split()) == 0:
             return Response(status=400)
 
         # Проверка уникальности, если такой юзер уже есть, возвращаю 409
